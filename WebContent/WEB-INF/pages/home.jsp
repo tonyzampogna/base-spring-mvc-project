93a681ff5b18511a556b1be07f90790d1a7a5f86
Tony Zampogna <tony.zampogna@target.com>
2015-04-17 12:53:13 -0500

    initial commit.

---
+++ WebContent/WEB-INF/pages/home.jsp
@@ -0,0 +1,7 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<body>
+	<h1>Spring MVC</h1>
+	<h3>Name: ${name}</h3>	
+</body>
+</html>
---
+++ src/com/tonyzampogna/spring/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.tonyzampogna.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("*")
+public class HomeController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("home");
+        mav.addObject("name", "Hello World!");
+		return mav;
+	}
+}
